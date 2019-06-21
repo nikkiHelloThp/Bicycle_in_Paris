a8592aae882ae1b1c5e0b7fba886b363ebabc996
nikkiHelloThp <nicolas.stanojevic.lpo@gmail.com>
2019-06-22 00:23:14 +0200

    rails d controller Home, add style to search form

--- app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-	def index
-		@availabilities = Availability.all
-		@thpaddress = Geocoder.search('9 rue Bergère, Paris 75002, France')
-		@thp = @thpaddress.first.coordinates
-		@closeststations = Availability.near(@thp, 1).take(5)
-	end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :locations
+  resources :locations, only: [:index]
 	root :to => "locations#index" # create availabilities: "availabilities#index"
 	resources :availabilities, only: [:index]
 end
