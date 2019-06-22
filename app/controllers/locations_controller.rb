069d45117379a6ab34ce6bf074f4999060f76647
nikkiHelloThp <nicolas.stanojevic.lpo@gmail.com>
2019-06-23 00:59:10 +0200

    add leaflet map + markers + popup

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,9 @@
 class LocationsController < ApplicationController
   def index
     if params[:search].present?
-      @locations = Availability.near(params[:search], 1, :order => :distance).take(5)
+    	@result = Geocoder.search(params[:search])
+      @result = @result.first.coordinates
+      @locations = Availability.near(@result, 1, :order => :distance).take(5)
     else
     	@thpaddress = Geocoder.search('9 rue Bergère, Paris 75002, France')
 			@thp = @thpaddress.first.coordinates
